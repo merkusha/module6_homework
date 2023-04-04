4da0c8ef1357289addef18dc31ca3c7e8cda0f32
merkusha <113843712+merkusha@users.noreply.github.com>
2023-04-04 23:18:25 +0300

    Add files via upload

---
+++ задание 4.js
@@ -0,0 +1,12 @@
+function countInOrderEverySecond(number1, number2) {
+    let progress = number1;
+
+    let intervalId = setInterval(function () {
+        if (progress <= number2) {
+            console.log(progress);
+            progress++;
+        }
+        else clearInterval(intervalId);
+    }, 1000);
+}
+countInOrderEverySecond(4, 20);
